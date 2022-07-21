685d89fdecb04d50066d805d26b2e3dbb08955b0
ShinjiFujita <fujita.shinji.0728@gmail.com>
2022-07-21 13:50:59 +0900

    neko

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Fujita Shinji",
+    version="0.1.0",
+    install_requires=[
+        "pandas",
+	"xarray",
+	"astropy",
+	"xarray_dataclasses", 
+	"python-casacore", 
+    ],
+    entry_points={
+        'console_scripts': [
+            'xmascat=xmascat:main',
+        ],
+    },
+    author="ShinjiFujita",
+    author_email="xxx",
+    description="Xarray to Mesumentset(v2) conversion module for ASte and other Common Astronomical Telescopes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShinjiFujita/xmascat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
---
+++ xmascat/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "Fujita Shinji"
+__version__ = "0.1.0"
