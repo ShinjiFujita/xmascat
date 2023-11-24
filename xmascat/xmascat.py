2afb18613ef5fbe9409989ac9d2b4cb26efb03bb
ShinjiFujita <fujita.shinji.0728@gmail.com>
2023-11-24 12:22:02 +0900

    neko

--- xmascat/xmascat.py
+++ xmascat/xmascat.py
@@ -419,6 +419,11 @@ def create_XFFTSxarray(path_startfile=None, path_antlogfile=None, path_XFFTSdata
 			except:
 				print("Please check the messfiles. No TOPO frequency information.")
 				return
+	try:
+		print("f_TOPO = ", f_TOPO, " Hz")
+	except:
+		print("Please check the messfiles. No TOPO frequency information!")
+		return
 	SET_dict, PTN_list = read_startfile(path_startfile)
 	if SET_dict["OTF_MODE"] == "ON":
 		obsmode = "OTF"
